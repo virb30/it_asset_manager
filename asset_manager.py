7ca8a6e615965d46e755e1762677179bd2b01dcf
Vinicius Boscoa <virb30@gmail.com>
2021-01-17 12:53:15 -0300

    add get_asset_info function

--- asset_manager.py
+++ asset_manager.py
@@ -1,20 +1,11 @@
-import psutil
-import cpuinfo
-from get_system_info import get_system_info
+from system_info import get_system_info
+from cpu_info import get_cpu_info
+
+
+def get_asset_info():
+    return {"system_info": get_system_info(), "cpu_info": get_cpu_info()}
 
 
 if __name__ == "__main__":
-    system_info = get_system_info()
-    print(system_info)
-
-    cpu = cpuinfo.get_cpu_info()
-    print(cpu["brand_raw"])
-
-    print(psutil.cpu_count(logical=True))
-    print(psutil.disk_partitions(all=True))
-    print(psutil.boot_time())
-    # print(psutil.net_connections())
-    # print(psutil.net_if_addrs())
-    print(psutil.virtual_memory())
-
     print("IT Asset Manager")
+    print(get_asset_info())
---
+++ tests/test_asset_manager.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from asset_manager import get_asset_info
+
+
+class AssetManagerTest(TestCase):
+    def setUp(self):
+        self.content = get_asset_info()
+
+    def test_get_asset_info(self):
+        self.assertTrue(self.content)
+
+    def test_asset_info_content(self):
+        """Asset info must contain keys"""
+        expected = ["system_info", "cpu_info"]
+
+        with self.subTest():
+            for key in expected:
+                self.assertIn(key, self.content)
